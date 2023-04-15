0bfe8de5104e5d211fa33d61235c3f08563c4a84
Mourier <fourier.mourier@gmail.com>
2023-04-15 20:31:21 +0300

    removed old lexicon as dict

--- lexicon/general.py
+++ lexicon/general.py
@@ -84,21 +84,6 @@ class LexiconModel:
 
 Lexicon = LexiconModel()
 
-LEXICON_EN: Dict[str, str] = {
-    '/start': (f"Hi, i will generate number and you will try to guess it\n"
-               f"you can also use commands /help or /stat"),
-    '/help': (f"Rules: I'll guess the number in range [1, 100] "
-              f"and you will try to guess;\n"
-              f"you have {GameConstants.attempts} attempts.\n"
-              f"Available commands:\n"
-              f"/help - rules & available commands,\n"
-              f"/cancel - to quit the game,\n"
-              f"/stat - get statistics"),
-    'no_game_yet': f"We haven't played yet. Shall we /start?",
-    'already_in_game': (f"We're already in the game. "
-                        f"I will respond only to numbers and command '/cancel' and '/stat'"),
-}
-
 
 def _main():
     lexicon = LexiconModel()
